520d4bd9e6ad49044d73eb5c878636aff4d17be2
Hazmi35 <hazminoname@gmail.com>
2018-10-26 15:13:42 +0700

    Release!

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const { get } = require('node-superfetch'); //the core of this package
 
-module.exports = () => {
-    var fetch = async () => { //create fetch function
-        var res = await get('https://aws.random.cat/meow'); //url of the api
-        if (res.ok) { //if the status code is 200 (OK), then return the body
-            return res.body;
-        } else { //if not
-            console.error('Error: Error while fetching cat image :\n'+ res.error); //return an error
-        }
-    }
-    var imageUrl = fetch().then(body => body.file); //get the image url
+module.exports = main;
 
-    return imageUrl; //return a image url
+/**
+ * Get a random image of a cat!
+ */
+function main() {
+    () => {
+        var fetch = async () => { //create fetch function
+            var res = await get('https://aws.random.cat/meow'); //url of the api
+            if (res.ok) { //if the status code is 200 (OK), then return the body
+                return res.body;
+            } else { //if not
+                console.error('Error: Error while fetching cat image :\n'+ res.error); //return an error
+            }
+        }
+        var imageUrl = fetch().then(body => body.file); //get the image url
+    
+        return imageUrl; //return a image url
+    }
 }
