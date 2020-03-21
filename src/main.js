6718c52b4ff7bf482bfc7fe2ca6cc66542259e37
Hazmi35 <contact@hzmi.xyz>
2020-03-21 11:58:50 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const request = new (require('./httpsClient'));
 
 /**
  * Get a random cat image
- * @returns {Promise} A promise that contains url to the image
+ * @returns {Promise<string>} A promise that contains url to the image
  */
 module.exports = function randomMeow() {
     return new Promise((resolve, reject) => {
