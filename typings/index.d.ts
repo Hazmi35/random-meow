466aa824a33f40189d12cd981c2072cae40df69e
Hazmi35 <contact@hzmi.xyz>
2020-10-28 06:20:38 +0700

    fix(typings): prettify typescript files

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,18 +1,18 @@
-declare module 'random-meow' {
-    import { IncomingHttpHeaders, RequestOptions } from "http";
+declare module "random-meow" {
+    import type { IncomingHttpHeaders, RequestOptions } from "http";
 
     /**
     * Get a random cat image
     * @returns {Promise<string>} A promise that contains url to the image
     */
-    function meow(): Promise<String>;
+    function meow(): Promise<string>;
 
     /**
     * Creates a GET request
     * @returns {Promise<{ method: "GET", reqOptions: RequestOptions, raw: string, body: any,status: number, headers: IncomingHttpHeaders }>} A Promise containing response
     * @private Internal Function - NO SUPPORT OUTSIDE RANDOM-MEOW
     */
-    function get(url: string, options?: RequestOptions): Promise<{ method: "GET", reqOptions: RequestOptions, raw: string, body: any,status: number, headers: IncomingHttpHeaders }>;
+    function get(url: string, options?: RequestOptions): Promise<{ method: "GET"; reqOptions: RequestOptions; raw: string; body: any;status: number; headers: IncomingHttpHeaders }>;
 
-    export default Object.assign(meow, {get});
+    export default Object.assign(meow, { get });
 }
