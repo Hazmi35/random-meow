6d732dfbe6a3365e197e03359efa884443dfea63
Hazmi35 <contact@hzmi.xyz>
2021-08-13 05:52:48 +0700

    feat: Try with HTTP if fails on HTTPS

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ const get = require("./createGetRequest");
 
 /**
  * Get a random cat image
+ * @param {boolean} [tryWithHttp] Whether to try with HTTP if fails on HTTPS (default: true)
  * @returns {Promise<string>} A promise that contains url to the image
  */
-function meow() {
+function meow(tryWithHttp = true) {
     return new Promise((resolve, reject) => {
-        get("https://aws.random.cat/meow").then(res => {
-            resolve(res.body.file);
-        }).catch(reject);
+        get("https://aws.random.cat/meow")
+            .then(res => resolve(res.body.file))
+            .catch(e => {
+                if (tryWithHttp) get("http://aws.random.cat/meow").then(res => resolve(res.body.file)).catch(reject);
+                else reject(e)
+            });
     });
 }
 
